90259321b27679c7e43efd02710a00375013d6de
Willem Elbers <willem.elbers@gmail.com>
2020-01-06 17:46:44 +0100

    Main program for exporting ncdm perturbation vector.

---
+++ ncdm_dump/compile.sh
@@ -0,0 +1,1 @@
+g++ dump_ncdm_perturb.cpp -o dump_ncdm -I../include ../libclass.a ../build/output.o -fopenmp -g -fPIC -O4 -ffast-math -I/usr/include/hdf5/serial -L/usr/lib/x86_64-linux-gnu/hdf5/serial -lhdf5_cpp -lhdf5
